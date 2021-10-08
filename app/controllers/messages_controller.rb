e49a7efb802248ee788633d8426fd55adba8e4e0
Karen Ventura <karen.ventura@michelada.io>
2021-10-07 22:12:40 -0500

    Use turbo streams using append action

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,7 +8,9 @@ class MessagesController < ApplicationController
   def create
     @message = @room.messages.create!(message_params)
 
+    #mime type
     respond_to do |format|
+      format.turbo_stream
       format.html { redirect_to @room }
     end
   end
